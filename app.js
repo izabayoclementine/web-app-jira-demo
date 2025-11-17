6ad3dea2da816d955b953442e08c762383e79dbc
izabayo_clementine <izabayoclementine0@gmail.com>
2025-11-17 19:24:44 +0200

    WEB-1 WEB-123: Create homepage route

--- app.js
+++ app.js
@@ -2,10 +2,17 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
+// Homepage route
 app.get("/", (req, res) => {
     res.send("Welcome to the Jira Integration Demo App");
+});
+
+// ⭐ New route example (e.g., for WEB-123)
+app.get("/home", (req, res) => {
+    res.send("This is the new homepage route created in a new branch.");
 });
 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
+
